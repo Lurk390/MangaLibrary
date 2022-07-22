b052b190ae8a04563674274b0ca8e3880f9018be
Lurk390 <elbasiouny.mahmoud@gmail.com>
2022-07-22 03:44:29 -0400

    Added print_table function and made SQL variables

--- src/main.py
+++ src/main.py
@@ -1,77 +1,41 @@
 """
 This module executes SQL queries to input data retrieved from get_manga_data() into the
-database.
+database. It also contains the function print_table() to print the contents of a table.
 
 @author: Mahmoud Elbasiouny
 """
 
 from dbconnection import connect
+from sqlqueries import INITIALIZE_TABLES, MAX_MANGAID_ROW
 from webscraper import get_manga_data
+
+
+def print_table(table_name):
+    """Prints the contents of a table. Must also be connected to the database.
+
+    Args:
+        table_name (string): Must be a table name that exists in the database
+    """
+
+    print(f"{table_name}:")
+    cursor.execute(f"""SELECT * FROM {table_name}""")
+    for row in cursor.fetchall():
+        print(row)
+    print("\n")
 
 
 # Connect to database
 cnxn = connect()
 cursor = cnxn.cursor()
 
-# Drops tables if they exist, then creates them
-# Manually inserts VolumeType and URL info for each manga into database
-cursor.execute(
-    """
-    IF (EXISTS (SELECT *
-                FROM INFORMATION_SCHEMA.TABLES
-                WHERE TABLE_NAME = 'MangaInfo'))
-    BEGIN
-        DROP TABLE MangaInfo;
-    END
-    ------------------------------------------------------------------------------------
-    CREATE TABLE MangaInfo (
-    MangaID INT IDENTITY,
-    Title VARCHAR(100),
-    Author VARCHAR(30),
-    Publisher VARCHAR(30),
-    Status VARCHAR(10),
-    VolumeType VARCHAR(10),
-    Year INT,
-    Description VARCHAR(MAX),
-    NumberOfVolumes INT,
-    CoverImage VARCHAR(100),
-    URL VARCHAR(100),
-    PRIMARY KEY(MangaID)
-    );
-
-    INSERT INTO MangaInfo (VolumeType, URL) VALUES('Single',
-                                                   'https://comicvine.gamespot.com/assassination-classroom/4050-78567/')
-    INSERT INTO MangaInfo (VolumeType, URL) VALUES('3-in-1',
-                                                   'https://comicvine.gamespot.com/berserk-deluxe-edition/4050-117309/')
-    INSERT INTO MangaInfo (VolumeType, URL) VALUES('Single',
-                                                   'https://comicvine.gamespot.com/berserk/4050-18867/')
-    INSERT INTO MangaInfo (VolumeType, URL) VALUES('Single',
-                                                   'https://comicvine.gamespot.com/chainsaw-man/4050-130799/')
-    INSERT INTO MangaInfo (VolumeType, URL) VALUES('Single',
-                                                   'https://comicvine.gamespot.com/dorohedoro/4050-32093/')
-    INSERT INTO MangaInfo (VolumeType, URL) VALUES('Single',
-                                                   'https://comicvine.gamespot.com/spy-x-family/4050-127643/')
-    INSERT INTO MangaInfo (VolumeType, URL) VALUES('Single',
-                                                   'https://comicvine.gamespot.com/the-promised-neverland/4050-106538/')
-    INSERT INTO MangaInfo (VolumeType, URL) VALUES('3-in-1',
-                                                   'https://comicvine.gamespot.com/uzumaki/4050-68740/')
-    INSERT INTO MangaInfo (VolumeType, URL) VALUES('2-in-1',
-                                                   'https://comicvine.gamespot.com/vinland-saga/4050-69157/')
-    """
-)
+# Creates tables if they don't exist
+cursor.execute(INITIALIZE_TABLES)
 
 # Finds last row in MangaInfo table and gets its MangaID
-MangaID_length = (
-    cursor.execute(
-        """
-        SELECT TOP 1 MangaID FROM MangaInfo ORDER BY MangaID DESC
-        """
-    ).fetchone()[0]
-    + 1
-)
+MangaID_length = cursor.execute(MAX_MANGAID_ROW).fetchone()[0]
 
 # Loops through all MangaInfo rows and gets URL from each row
-for i in range(1, MangaID_length):
+for i in range(1, MangaID_length + 1):
     url = cursor.execute(
         f"""
         SELECT URL FROM MangaInfo WHERE MangaID = {i}
@@ -92,26 +56,27 @@ for i in range(1, MangaID_length):
         CoverImage = ?
         WHERE MangaID = ?
         """,
-        (manga_data["title"],
-         manga_data["author"],
-         manga_data["year"],
-         manga_data["publisher"],
-         manga_data["number of volumes"],
-         manga_data["description"],
-         manga_data["status"],
-         manga_data["cover image"],
-         i,)
+        (
+            manga_data["title"],
+            manga_data["author"],
+            manga_data["year"],
+            manga_data["publisher"],
+            manga_data["number of volumes"],
+            manga_data["description"],
+            manga_data["status"],
+            manga_data["cover image"],
+            i,
+        ),
     )
 
+# ======================================================================================
+# Create a for loop that inserts MangaID and VolumeNumber into the VolumeInfo table for
+# each series in the MangaInfo table.
+# ======================================================================================
+
 # Print MangaInfo table
-cursor.execute(
-    """
-    SELECT * FROM MangaInfo
-    """
-)
-for row in cursor.fetchall():
-    print(row)
-    print("\n")
+print_table("MangaInfo")
+print_table("VolumeInfo")
 
 # Commit changes and close connection
 cnxn.commit()
