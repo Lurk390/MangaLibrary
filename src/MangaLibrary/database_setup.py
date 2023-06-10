3f692693e9cfb3d69a52a8266a501bbb8be5268a
Lurk390 <elbasiouny.mahmoud@gmail.com>
2023-06-10 04:07:39 -0400

    removed redundant comments

--- src/MangaLibrary/database_setup.py
+++ src/MangaLibrary/database_setup.py
@@ -2,20 +2,17 @@
 
 
 INITIALIZE_TABLES = """
--- Drop tables if they exist
 DROP TABLE IF EXISTS Users;
 DROP TABLE IF EXISTS MangaInfo;
 DROP TABLE IF EXISTS VolumeInfo;
 DROP TABLE IF EXISTS UserToVolume;
 
--- Create Users table
 CREATE TABLE Users (
     UserID INTEGER PRIMARY KEY AUTOINCREMENT,
     FirstName TEXT NOT NULL,
     LastName TEXT NOT NULL
 );
 
--- Create MangaInfo table
 CREATE TABLE MangaInfo (
     MangaID INTEGER PRIMARY KEY AUTOINCREMENT,
     Title TEXT,
@@ -30,14 +27,12 @@ CREATE TABLE MangaInfo (
     URL TEXT
 );
 
--- Create VolumeInfo table
 CREATE TABLE VolumeInfo (
     VolumeID INTEGER PRIMARY KEY AUTOINCREMENT,
     MangaID INTEGER NOT NULL,
     VolumeNumber INTEGER NOT NULL
 );
 
--- Create UserToVolume table
 CREATE TABLE UserToVolume (
     UserToVolumeID INTEGER PRIMARY KEY AUTOINCREMENT,
     UserID INTEGER NOT NULL,
