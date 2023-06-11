fa4001a57cbf7ebb7defee383da123e80e2b38a4
Mahmoud Elbasiouny <elbasiouny.mahmoud@gmail.com>
2023-06-11 04:16:56 -0400

    oopified database functions together and simplified main

---
+++ src/MangaLibrary/database_functions.py
@@ -0,0 +1,107 @@
+import os
+import sqlite3
+
+from MangaLibrary.manga_series import MangaSeries
+
+
+class DatabaseFunctions:
+    """Class that handles all database functions"""
+
+    def __init__(self):
+        if not os.path.exists("data"):
+            os.makedirs("data")
+
+        self.connection = sqlite3.connect("data/library.db")
+        self.cursor = self.connection.cursor()
+
+        self.init_tables()
+
+    def __del__(self):
+        self.connection.close()
+
+    def init_tables(self) -> None:
+        """Creates all tables if they don't exist"""
+
+        self.cursor.executescript(
+            """
+            DROP TABLE IF EXISTS Users;
+            DROP TABLE IF EXISTS MangaInfo;
+            DROP TABLE IF EXISTS VolumeInfo;
+            DROP TABLE IF EXISTS UserToVolume;
+            
+            CREATE TABLE Users (
+                UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+                FirstName TEXT NOT NULL,
+                LastName TEXT NOT NULL
+            );
+            
+            CREATE TABLE MangaInfo (
+                MangaID INTEGER PRIMARY KEY AUTOINCREMENT,
+                Title TEXT,
+                Author TEXT,
+                Publisher TEXT,
+                Status TEXT,
+            --    VolumeType TEXT,
+                Year INTEGER,
+                Description TEXT,
+                NumberOfVolumes INTEGER,
+                CoverImage TEXT,
+                URL TEXT
+            );
+            
+            CREATE TABLE VolumeInfo (
+                VolumeID INTEGER PRIMARY KEY AUTOINCREMENT,
+                MangaID INTEGER NOT NULL,
+                VolumeNumber INTEGER NOT NULL
+            );
+            
+            CREATE TABLE UserToVolume (
+                UserToVolumeID INTEGER PRIMARY KEY AUTOINCREMENT,
+                UserID INTEGER NOT NULL,
+                VolumeID INTEGER NOT NULL
+            );
+            """
+        )
+
+    def add_manga(self, manga_series: MangaSeries) -> None:
+        """Adds the data from a MangaSeries object into the database
+
+        Args:
+            manga_series (MangaSeries): A MangaSeries object
+        """
+
+        self.cursor.execute(
+            """
+            INSERT INTO MangaInfo (Title, Author, Year, Publisher, NumberOfVolumes,
+                                   Description, Status, CoverImage, URL)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+            """,
+            (
+                manga_series.title,
+                manga_series.author,
+                manga_series.year,
+                manga_series.publisher,
+                manga_series.number_of_volumes,
+                manga_series.description,
+                manga_series.status,
+                manga_series.cover_image,
+                manga_series.url,
+            ),
+        )
+        self.connection.commit()
+
+    def add_user(self, first_name: str, last_name: str) -> None:
+        """Adds a user into the database
+
+        Args:
+            first_name (str): User's first name
+            last_name (str): User's last name
+        """
+        self.cursor.execute(
+            """
+            INSERT INTO Users (FirstName, LastName)
+            VALUES (?, ?)
+            """,
+            (first_name, last_name),
+        )
+        self.connection.commit()
--- src/MangaLibrary/main.py
+++ src/MangaLibrary/main.py
@@ -1,16 +1,8 @@
-"""This module executes SQL queries to input data retrieved from get_manga_data() into
-the database. It also contains the function print_table() to print the contents of a
-table.
-"""
-import os
-import sqlite3
-
-from src.MangaLibrary.database_setup import INITIALIZE_TABLES
-from src.MangaLibrary.manga_series import MangaSeries
+from MangaLibrary.database_functions import DatabaseFunctions
+from MangaLibrary.manga_series import MangaSeries
 
 
 def main():
-    # List of manga to test the program
     TEST_HARNESS = [
         "Assassination Classroom",
         "Berserk Deluxe Edition",
@@ -23,38 +15,8 @@ def main():
         "Vinland Saga",
     ]
 
-    # Check if data folder exists and connect to database
-    if not os.path.exists("data"):
-        os.makedirs("data")
-    connection = sqlite3.connect("data/library.db")
-    cursor = connection.cursor()
+    db = DatabaseFunctions()
 
-    # Creates tables if they don't exist
-    cursor.executescript(INITIALIZE_TABLES)
-
-    # Loops through TEST_HARNESS and inserts data from get_manga_data into MangaInfo
     for manga in TEST_HARNESS:
         manga_data = MangaSeries(manga)
-        cursor.execute(
-            """
-            INSERT INTO MangaInfo (Title, Author, Year, Publisher, NumberOfVolumes,
-                                   Description, Status, CoverImage, URL)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
-            """,
-            (
-               manga_data.title,
-               manga_data.author,
-               manga_data.year,
-               manga_data.publisher,
-               manga_data.number_of_volumes,
-               manga_data.description,
-               manga_data.status,
-               manga_data.cover_image,
-               manga_data.url,
-            ),
-            )
-
-    # Commit changes and close connection
-    connection.commit()
-    cursor.close()
-    connection.close()
+        db.add_manga(manga_data)
