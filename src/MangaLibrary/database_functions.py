08365ff51cbc699fd7b5fc6254290963225cb6c7
Mahmoud Elbasiouny <elbasiouny.mahmoud@gmail.com>
2023-06-25 01:32:19 -0400

    populate_volumes functions to add rows to Volumes table

--- src/MangaLibrary/database_functions.py
+++ src/MangaLibrary/database_functions.py
@@ -23,7 +23,7 @@ class DatabaseFunctions:
             """
             DROP TABLE IF EXISTS Users;
             DROP TABLE IF EXISTS MangaInfo;
-            DROP TABLE IF EXISTS VolumeInfo;
+            DROP TABLE IF EXISTS Volumes;
             DROP TABLE IF EXISTS UserToVolume;
             
             CREATE TABLE Users (
@@ -47,7 +47,7 @@ class DatabaseFunctions:
                 URL TEXT
             );
             
-            CREATE TABLE VolumeInfo (
+            CREATE TABLE Volumes (
                 VolumeID INTEGER PRIMARY KEY AUTOINCREMENT,
                 MangaID INTEGER NOT NULL,
                 VolumeNumber INTEGER NOT NULL
@@ -60,6 +60,29 @@ class DatabaseFunctions:
             );
             """
         )
+
+    def populate_volumes(self, manga_series: str) -> None:
+        """Populates the Volumes table with all volumes from the MangaInfo table"""
+
+        self.cursor.execute(
+            """
+            SELECT MangaID, NumberOfVolumes
+            FROM MangaInfo
+            WHERE Title = ?
+            """,
+            (manga_series,),
+        )
+
+        manga_id, number_of_volumes = self.cursor.fetchone()
+
+        for volume_number in range(1, number_of_volumes + 1):
+            self.cursor.execute(
+                """
+                INSERT INTO Volumes (MangaID, VolumeNumber)
+                VALUES (?, ?)
+                """,
+                (manga_id, volume_number),
+            )
 
     def add_manga(self, manga_series: MangaSeries) -> None:
         """Adds the data from a MangaSeries object into the database
@@ -86,6 +109,8 @@ class DatabaseFunctions:
                 manga_series.url,
             ),
         )
+
+        self.populate_volumes(manga_series.title)
         self.connection.commit()
 
     def add_user(self, username: str, first_name: str, last_name: str) -> None:
