8ec3abe9348bdf0234afea671d01f52ce155affe
Mahmoud Elbasiouny <elbasiouny.mahmoud@gmail.com>
2023-06-26 01:01:29 -0400

    added specific volumes

--- src/MangaLibrary/main.py
+++ src/MangaLibrary/main.py
@@ -9,6 +9,7 @@ def main():
         "Berserk",
         "Chainsaw Man",
         "Dorohedoro",
+        "Fire Punch",
         "Spy x Family",
         "The Promised Neverland",
         "Uzumaki",
@@ -22,5 +23,24 @@ def main():
     for manga in TEST_HARNESS:
         manga_data = MangaSeries(manga)
         db.add_manga(manga_data)
+
+    volumes = {
+        "Berserk Deluxe Edition": [1, 2],
+        "Uzumaki": [1],
+        "Vinland Saga": [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12],
+        "Assassination Classroom": list(range(1, 13)),
+        "Dorohedoro": [1, 2],
+        "Fire Punch": [1, 3, 5],
+        "Spy x Family": list(range(1, 10)),
+        "The Promised Neverland": list(range(1, 21)),
+    }
+
+    for manga, volumes_list in volumes.items():
+        for volume in volumes_list:
+            db.add_volume_to_user("Lurk390", manga, volume)
+
+    print("\nLurk390's manga:")
+    for x in db.get_user_volumes("Lurk390"):
+        print(x)
 
     db.connection.close()
